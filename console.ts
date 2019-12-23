2e95724aebc6cf7bb88459a1c03a057d948dfbe9
kaddo <kofi.addo@facile.it>
2019-12-23 17:37:32 +0100

    feat: bookapifetcher add

--- console.ts
+++ console.ts
@@ -1,21 +1,24 @@
 import commander from "commander";
-import { PacktPubClient } from "./src/Service/PacktPub/PacktPubClient";
+import {BookAPIFetcher} from "./src/Book/Fetcher/BookAPIFetcher"
+import {Book} from "./src/Book/Entity/Book";
+import {BookBuilder} from "./src/Book/Builder/BookBuilder";
+import {PacktPubClient} from "./src/Service/PacktPub/PacktPubClient";
+
 commander.version("1.0.0").description("Bot retrieve free books");
 
 commander
-  .command("send")
-  .alias("s")
-  .description("greet in cnsole")
-  .action(() => {
-    let packtPubClient = new PacktPubClient();
-    let todaysOfferResponse = packtPubClient.fetchTodayOffer();
-    todaysOfferResponse
-      .then(function(result) {
-        console.log(result);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  });
+    .command("send")
+    .alias("s")
+    .description("greet in cnsole")
+    .action(() => {
+        let bookApifetcher = new BookAPIFetcher(new PacktPubClient, new BookBuilder).fetch();
+        bookApifetcher
+            .then(function (result: Book) {
+                console.log(result.toString());
+            })
+            .catch(function (error: Book) {
+                console.log(error);
+            });
+    });
 
 commander.parse(process.argv);
