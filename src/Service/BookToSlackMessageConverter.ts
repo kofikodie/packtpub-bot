9391d96ab7a5fa3464fd91b005514c7de768eca6
kaddo <kofi.addo@facile.it>
2019-12-28 17:15:43 +0100

    feat: convert method update

--- src/Service/BookToSlackMessageConverter.ts
+++ src/Service/BookToSlackMessageConverter.ts
@@ -1,30 +1,54 @@
 import {Book} from "../Book/Entity/Book";
+import {Author} from "../Book/Entity/Author";
 
 export class BookToSlackMessageConverter {
     convert(book: Book): JSON {
-        let messageData = [];
-        messageData[0] = this.buildTitleBlock();
-        messageData[1] = this.buildInfoBlock(book);
-        messageData[2] = this.buildButtonsBlock();
-        return JSON.parse(JSON.stringify(messageData));
-    }
+        let bookInfo = `*${book.title}*\n`;
+        book.author.forEach((author: Author) => {
+            bookInfo += `_${author.name}_\n`;
+        });
+        bookInfo += '\n';
+        bookInfo += `${book.publicationDate}\n`;
+        bookInfo += `${book.description}\n`;
 
-    private buildTitleBlock() {
-
-        return {
-            "type": "section",
-            'text': {
-                'type': 'plain_text',
-                'text': "Today's Free eBook:",
-            }
+        let messageData = {
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "Today's Free eBook:"
+                    }
+                },
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `${bookInfo}`
+                    },
+                    "accessory": {
+                        "type": "image",
+                        "image_url": `${book.coverURL}`,
+                        "alt_text": `${book.title}`
+                    }
+                },
+                {
+                    "type": "actions",
+                    "elements": [
+                        {
+                            "type": "button",
+                            "text": {
+                                "type": "plain_text",
+                                "text": "Button",
+                                "emoji": true
+                            },
+                            "style": "primary",
+                            "url": "https://www.packtpub.com/free-learning"
+                        }
+                    ]
+                }
+            ]
         };
-    }
-
-    private buildInfoBlock(book: Book) {
-        return [""];
-    }
-
-    private buildButtonsBlock() {
-        return [""]
+        return JSON.parse(JSON.stringify(messageData));
     }
 }
