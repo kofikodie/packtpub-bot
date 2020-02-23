d6a99b681a40a7b6fa677948708cd1f47e0fc8fb
kaddo <kofi.addo@facile.it>
2020-02-23 19:01:29 +0100

    feat: private fields

--- src/Book/Builder/BookBuilder.ts
+++ src/Book/Builder/BookBuilder.ts
@@ -3,51 +3,51 @@ import { BookBuilderInterface } from "./BookBuilderInterface";
 import { Author } from "../Entity/Author";
 
 export class BookBuilder implements BookBuilderInterface {
-  private _id: number;
-  private _title: string;
-  private _authors: Author[];
-  private _publicationDate: string;
-  private _description: string;
-  private _coverURL: string;
+  #id: number;
+  #title: string;
+  #authors: Author[];
+  #publicationDate: string;
+  #description: string;
+  #coverURL: string;
 
   id(id: number): BookBuilder {
-    this._id = id;
+    this.#id = id;
     return this;
   }
 
   title(title: string): BookBuilder {
-    this._title = title;
+    this.#title = title;
     return this;
   }
 
   author(authors: Author[]): BookBuilder {
-    this._authors = authors;
+    this.#authors = authors;
     return this;
   }
 
   publicationDate(publicationDate: string): BookBuilder {
-    this._publicationDate = publicationDate;
+    this.#publicationDate = publicationDate;
     return this;
   }
 
   description(description: string): BookBuilder {
-    this._description = description;
+    this.#description = description;
     return this;
   }
 
   coverURL(coverURL: string): BookBuilder {
-    this._coverURL = coverURL;
+    this.#coverURL = coverURL;
     return this;
   }
 
   build(): Book {
     return new Book(
-      this._id,
-      this._title,
-      this._authors,
-      this._publicationDate,
-      this._description,
-      this._coverURL
+      this.#id,
+      this.#title,
+      this.#authors,
+      this.#publicationDate,
+      this.#description,
+      this.#coverURL
     );
   }
 }
