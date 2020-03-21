bb67f197d547f67b0aa54257549766796ddf9f5a
kaddo <kofi.addo@facile.it>
2020-03-21 02:04:03 +0100

    feat: axios instance

--- src/Service/PacktPub/PacktPubClient.ts
+++ src/Service/PacktPub/PacktPubClient.ts
@@ -1,39 +1,52 @@
 import axios from "axios";
 import moment from "moment";
-import { PacktPubInterface } from "./PacktPubInterface";
-import { IFetchBook } from "./Interface/IFetchBook";
-import { IFetchAuthor } from "./Interface/IFetchAuthor";
-import { IFetchTodayOffer } from "./Interface/IFetchTodayOffer";
+import {PacktPubInterface} from "./PacktPubInterface";
+import {IBook} from "./Interface/IBook";
+import {IAuthor} from "./Interface/IAuthor";
+import {ITodayOffer, OfferType} from "./Interface/ITodayOffer";
 
 export class PacktPubClient implements PacktPubInterface {
-    #OFFER_URL: string =
-    "https://services.packtpub.com/free-learning-v1/offers";
-    #BOOK_URL: string = "https://static.packt-cdn.com/products";
-    #AUTHOR_URL: string = "https://static.packt-cdn.com/authors";
+  readonly #OFFER_URL: string =
+  "https://services.packtpub.com/free-learning-v1/offers";
+  readonly #BOOK_URL: string = "products";
+  readonly #AUTHOR_URL: string = "authors";
+  readonly #apiClient = axios.create({
+    baseURL: "https://static.packt-cdn.com/",
+    responseType: "json",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
 
   async fetchCoverURLByBookId(id: string): Promise<string> {
     return `https://static.packt-cdn.com/products/${id}/cover/smaller`;
   }
 
-  async fetchAuthorById(id: string): Promise<IFetchAuthor> {
-    let response = await axios.get(`${this.#AUTHOR_URL}/${id}`);
+  async fetchAuthorById(id: string): Promise<IAuthor> {
+    const response = await this.#apiClient.get<IAuthor>(
+        `https://static.packt-cdn.com/${this.#AUTHOR_URL}/${id}`
+    );
     return response.data;
   }
 
-  async fetchBookById(id: string): Promise<IFetchBook> {
-    let response = await axios.get(`${this.#BOOK_URL}/${id}/summary`);
+  async fetchBookById(id: string): Promise<IBook> {
+    const response = await this.#apiClient.get<IBook>(
+        `${this.#BOOK_URL}/${id}/summary`
+    );
     return response.data;
   }
 
-  async fetchTodayOffer(): Promise<IFetchTodayOffer> {
+  async fetchTodayOffer(): Promise<string> {
     let todaysDateFormated: string = moment(new Date()).format("YYYY-MM-DD");
     let tommorrowDateFormated: string = moment(new Date())
       .add(1, "days")
       .format("YYYY-MM-DD");
 
-    let response = await axios.get(
-      `${this.#OFFER_URL}?dateFrom=${todaysDateFormated}&dateTo=${tommorrowDateFormated}`
+    const response = await this.#apiClient.get<ITodayOffer>(
+        `${this.#OFFER_URL}/?dateFrom=${todaysDateFormated}&dateTo=${tommorrowDateFormated}`
     );
-    return response.data;
+    let bookID: string;
+    response.data.data.forEach((offer: OfferType) => bookID = offer.productId);
+    return bookID;
   }
 }
